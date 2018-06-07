da26076d157b653f868bfa78a3bc99cf416b556f
mahab basha <moubasha786@gmail.com>
2018-06-07 23:57:51 +0530

    My Project Setup completed successfully

---
+++ shoppingbackend/src/main/java/com/mahab/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.mahab.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
